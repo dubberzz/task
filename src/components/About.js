f70b021f6e6c118eb3db2ea532eab097c3a90192
dubberzz <99068765+dubberzz@users.noreply.github.com>
2022-04-25 19:24:07 +0300

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div>
+        <h4>Versiunea 1.0.0</h4>
+        <h4>Baltariu George</h4>
+        <Link to="/">Inapoi</Link>
+    </div>
+  )
+}
+
+export default About
